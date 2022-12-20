d6c54f835788ce5fac3bcf8691a892810ad48979
Alexander Schilling <alexander.schilling@students.fhnw.ch>
2022-12-20 18:22:34 +0100

    update helper file

--- shits.py
+++ shits.py
@@ -6,6 +6,13 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import TruncatedSVD
+import gensim
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+from sklearn.model_selection import train_test_split
+from gensim.utils import simple_preprocess
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 def lsa_final(liked, movies, cols):
@@ -57,7 +64,6 @@ def lsa_final(liked, movies, cols):
 
 def tfidf_rec_user(df, split, cols):
     # create tfidf column with column combination
-
     df["tfidf"] = df[cols].apply(" ".join, axis=1)
 
     # create cosine similarity matrix
@@ -83,108 +89,125 @@ def tfidf_rec_user(df, split, cols):
         # remove duplicates but keep the order
         movie_lst = list(dict.fromkeys(movie_lst))
 
-        # remove where there is a movie multiple times
-        testing_movies[key] = movie_lst[:20]
+        testing_movies[key] = movie_lst[:5]
 
     return testing_movies
 
 
 def lsa_rec_user(df, split, cols):
 
-
-
     df["LSA"] = df[cols].apply(" ".join, axis=1)
-
     titles = df['title'].tolist()
-
     descriptions = df['LSA'].tolist()
 
-
-
     # Verwenden Sie TfidfVectorizer, um die Titel und Beschreibungen der Filme in Vektoren umzuwandeln
-
     vectorizer = TfidfVectorizer()
-
     vectors = vectorizer.fit_transform(descriptions)
 
-
-
     # Führen Sie LSA durch, indem Sie TruncatedSVD verwenden
-
     svd = TruncatedSVD(n_components=100)
-
     latent_vectors = svd.fit_transform(vectors)
-
     testing_movies = {}
 
     cnt = 0
-
     for key, values in split.items():
-
         cnt += 1
-
         movie_lst = []
-
         for value in values:
-
         # Finden Sie den Film, dessen Titel dem gegebenen Titel entspricht
-
             if value not in titles:
-
                 print('Film not found in DataFrame')
-
             else:
-
                 index = titles.index(value)
-
                 if index >= len(df):
-
                     print('Index not found in DataFrame')
-
                 else:
-
                     movie_vector = latent_vectors[index]
 
-
-
         # Berechnen Sie die Cosinus-Ähnlichkeit zwischen dem Film und allen anderen Filmen
-
             movie_vector = movie_vector.reshape(1, -1)
-
             latent_vectors = latent_vectors.reshape(latent_vectors.shape[0], -1)
-
             similarities = cosine_similarity(movie_vector, latent_vectors)
-
             movie_lst.append(similarities[0][similarities[0].argsort()[::-1]])
 
-
-
         # flatten list
-
         movie_lst = [item for sublist in movie_lst for item in sublist]
 
-
-
         # sort list on score
-
         movie_lst = [i for i in similarities[0].argsort()[::-1]][1:6]
 
-
-
-
         # remove duplicates but keep the order
-
         movie_lst = [titles[i] for i in movie_lst]
-
-
-
-        # remove where there is a movie multiple times
 
         testing_movies[key] = movie_lst[:20]
 
         print(cnt)
 
 
+
+    return testing_movies
+
+
+def tagged_document(list_of_list_of_words, titles):
+   for i, list_of_words in enumerate(list_of_list_of_words):
+      yield gensim.models.doc2vec.TaggedDocument(list_of_words.split(), [titles[i]])
+
+
+def doc2vec_rec_user(df, split, cols):
+    # create tfidf column with column combination
+    df["doc2vec"] = df[cols].apply(" ".join, axis=1)
+
+    tagged_document = list(tagged_document(df["doc2vec"], df['movieId']))
+
+    model = Doc2Vec(tagged_document, vector_size=90, window=5, min_count=1, workers=4)
+
+    testing_movies = {}
+
+    count = 0
+    for key, values in split.items():
+        count += 1
+        print(count)
+        movie_lst = []
+        for value in values:
+            # get id of movie title
+            movie_id = df[df["title"] == value]["movieId"].values[0]
+
+            # from tagged_documents get words where tags = movie_id
+            word_list = [word for word, tag in tagged_document if tag == movie_id]
+
+            # flatten list
+            word_list = [item for sublist in word_list for item in sublist]
+
+            # infer vector of movie overview
+            word_list_vectorized = model.infer_vector(word_list)
+
+            # get cosine similarity of movie overview with all other movie overviews
+            similar_sentences = model.docvecs.most_similar([word_list_vectorized], topn = 6)
+
+            # sort by score
+            similar_sentences = sorted(similar_sentences, key = lambda x: x[1], reverse = True)
+
+            # exclude 1st recommendation (movie itself)
+            similar_sentences = similar_sentences[1:]
+
+            # get movie ids of similar sentences
+            similar_sentences = [movie_id for movie_id, score in similar_sentences]
+
+            # get movie titles of similar sentences
+            recommendations = df[df["movieId"].isin(similar_sentences)]["title"].values
+
+            movie_lst.append(recommendations)
+
+        # flatten list
+        movie_lst = [item for sublist in movie_lst for item in sublist]
+
+        # sort list on score
+        movie_lst = sorted(movie_lst, key=lambda x: x[1], reverse=True)
+
+        # remove the score from movie list but keep the order
+        movie_lst = [item[0] for item in movie_lst]
+
+        testing_movies[key] = movie_lst[:5]
 
     return testing_movies
 
@@ -263,8 +286,6 @@ def stemming_tokenizer(data):
   for overview in data:
     # remove all non-alphabetic characters
     overview = re.sub(r'[^a-zA-Z]', ' ', overview)
-
-    # remove
 
     # remove single words
     overview = re.sub(r'\b[a-zA-Z]\b', ' ', overview)
